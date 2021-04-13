f9bde3d477b045da91e7f9b8413c740a75e7280a
Dunieski Otano <dunieskior@gmail.com>
2021-04-13 10:50:45 -0400

    Working with Functions

--- Lesson 6- Working with Functions.sql
+++ Lesson 6- Working with Functions.sql
@@ -28,6 +28,11 @@ INSERT INTO product (product_name, product_manufacturer, product_price) VALUES
 ('HP XPS360', 'HP', 2400.99),
 ('Lenovo', 'Lenovo', 2000.99)
 
+-- Add two duplicate rows
+INSERT INTO product (product_name, product_manufacturer, product_price) VALUES
+('HP XPS360', 'HP', 2400.99),
+('Lenovo', 'Lenovo', 2000.99)
+
 INSERT INTO time_test VALUES (CURDATE(), '2020-10-20')
 -- --- END OF INSERT DATA SECTION ---
 
@@ -35,6 +40,9 @@ INSERT INTO time_test VALUES (CURDATE(), '2020-10-20')
 SELECT CURDATE(), DATE(NOW()), CURTIME(), TIME(NOW())
 
 -- MANIPULATE DATA FROM product TABLE
+
+-- Query the entire table
+SELECT * FROM product 
 
 -- Return number of products (rows/records) with the function COUNT() [from product]
 SELECT COUNT(*) AS "No of Products" FROM product 
@@ -50,3 +58,52 @@ SELECT MIN(product_price) AS "Minimum Product Price" FROM product
 
 -- Returns the total of prices with function SUM()
 SELECT SUM(product_price) AS "Total Product Price" FROM product 
+
+-- Return unique values with the DISTINCT keyword
+SELECT DISTINCT product_name, product_price, product_manufacturer FROM product 
+
+-- Return number of unique rows
+SELECT COUNT(product_id) AS "No of Rows", COUNT(DISTINCT product_name) "No of Unique Rows" FROM product 
+
+-- Return all products manufactured by Apple
+SELECT * 
+FROM product 
+WHERE product_manufacturer = 'Apple'
+
+-- Return all the lengths of product names with the function LENGTH() 
+SELECT LENGTH(product_name) "Product Name Length" FROM product 
+
+-- Return all names in all uppercase and lowercase with UPPER() and LOWER() functions 
+SELECT 
+UPPER(product_name) AS "Names in Upper Case", 
+LOWER(product_name) AS "Names in Lower Case" 
+FROM product 
+
+-- Similar to UPPER()
+SELECT UCASE('The United States') 
+-- Similar to LOWER()
+SELECT LCASE('The United States')
+
+-- CONCAT()
+SELECT @maxprice := MAX(product_price) FROM product 
+
+SELECT DISTINCT @maxprice FROM product p 
+
+
+SELECT CONCAT(
+'Product Name: ', product_name, 
+',    Product Price: $', product_price, 
+',    Quantity Purchased: 2,    Subtotal: $', product_price * 2,
+',    Taxes: $', ROUND((product_price * 2 + product_price * 0.07), 2), 
+',    Grand Total: $', ROUND((product_price * 2 + (product_price * 2 + product_price * 0.07)), 2)) FROM product  
+
+
+-- Remove trailing spaces with the function RTRIM()
+SELECT CONCAT(RTRIM('Cuba   '), ' is great') 
+
+-- Remove leading spaces with the function LTRIM()
+SELECT CONCAT(LTRIM('                         Cuba'), ' is great')
+
+-- Remove all spaces (leading and trailing) with the function TRIM()
+SELECT CONCAT('-', TRIM('            Cuba             '), '-')
+-- Returns -Cuba-
