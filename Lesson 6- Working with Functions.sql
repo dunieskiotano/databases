e7bc5aecf093861d165f9b437002b523ba8a78c5
Dunieski Otano <dunieskior@gmail.com>
2021-04-13 09:52:52 -0400

    Working with Functions

--- Lesson 6- Working with Functions.sql
+++ Lesson 6- Working with Functions.sql
@@ -20,9 +20,9 @@ CREATE TABLE time_test
 
 -- --- INSERT DATA SECTION ---
 INSERT INTO product (product_name, product_manufacturer, product_price) VALUES
-('MacBook Pro', 'Apple', 4500.99),
+('MacBook Pro', 'Apple', 4500.99), -- max price
 ('MacBook Air', 'Apple', 1500.99),
-('iPad Pro', 'Apple', 1200.99),
+('iPad Pro', 'Apple', 1200.99), -- min price
 ('Microsoft Surface', 'Microsoft', 1400.99),
 ('Dell', 'Dell', 1600.99),
 ('HP XPS360', 'HP', 2400.99),
@@ -36,3 +36,17 @@ SELECT CURDATE(), DATE(NOW()), CURTIME(), TIME(NOW())
 
 -- MANIPULATE DATA FROM product TABLE
 
+-- Return number of products (rows/records) with the function COUNT() [from product]
+SELECT COUNT(*) AS "No of Products" FROM product 
+
+-- Returns average of prices with the function AVG() and ROUND()
+SELECT ROUND(AVG(product_price), 2) AS "Product Price Average" FROM product
+
+-- Returns product with the highest price with function MAX()
+SELECT MAX(product_price) AS "Maximum Product Price" FROM product 
+
+-- Returns product with the lowest price with function MIN()
+SELECT MIN(product_price) AS "Minimum Product Price" FROM product 
+
+-- Returns the total of prices with function SUM()
+SELECT SUM(product_price) AS "Total Product Price" FROM product 
