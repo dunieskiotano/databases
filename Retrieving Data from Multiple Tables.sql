15e6267f9d8f832cd91322b0f17a0e4ff1ba7da8
Dunieski Otano <dunieskior@gmail.com>
2021-04-14 09:10:13 -0400

    Retrieving Data from Multiple Tables

--- Retrieving Data from Multiple Tables.sql
+++ Retrieving Data from Multiple Tables.sql
@@ -23,3 +23,19 @@ CREATE TABLE staff
 	phone TEXT NOT NULL,
 	manager_id INT NULL
 )
+
+
+-- INSERT SECTION
+INSERT INTO new_movies (movie_director, movie_main_actor) VALUES
+('Oliver Stone', 'Robert de Niro'),
+('Oliver Stone', 'Tom Hanks'),
+('Oliver Stone', 'Nicholas Cage'),
+('James Cameron', 'Bruce Willis'),
+('Steven Spielberg', 'Jennifer Lopez')
+
+INSERT INTO old_movies (movie_name, movie_release_date) VALUES
+('Gone with the wind', '1937-10-10'),
+('The Wild West', '1920-09-09'),
+('The Beauty and the Beast', '1950-07-07'),
+('Billy The Beast', '1900-01-01'),
+('Why are you so mean to me?', '1810-01-02')
